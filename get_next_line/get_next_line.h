cef121a4982b59a76656837acd9c7850216c9072
Omar Iskandarani <oiskanda@c1r5s4.42beirut.com>
2025-07-31 23:55:03 +0300

    adding files needed for project

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oiskanda <oiskanda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 01:17:33 by oiskanda          #+#    #+#             */
+/*   Updated: 2025/05/27 13:19:01 by oiskanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_copy_to_newline(char *str);
+char	*ft_copy_left_from_buffer(char *str);
+char	*get_next_line(int fd);
+
+#endif
